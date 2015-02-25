6fc7b75488fd5134fc305f183ab7f5a132b5cf4a
David Benitez <debml700@gmail.com>
2015-02-24 20:47:08 -0600

    Se agregó metodos run, actualiza, checaColision, paint, paint1. NOTA: Ambos paints siguen adaptados al juego anterior

--- src/breakingbad/BreakingBad.java
+++ src/breakingbad/BreakingBad.java
@@ -5,12 +5,128 @@
  */
 package breakingbad;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
 /**
  *
- * @author Patricio
+ * @author Patricio Sanchez and David Benítez 
  */
 public class BreakingBad {
+    
+    public BreakingBad () {
+        
+    }
+    
+    public void run () {
+        /* mientras dure el juego, se actualizan posiciones de jugadores
+           se checa si hubo colisiones para desaparecer jugadores o corregir
+           movimientos y se vuelve a pintar todo
+        */ 
+        while (true) {
+            actualiza();
+            checaColision();
+            repaint();
+            try	{
+                // El thread se duerme.
+                Thread.sleep (20);
+            }
+            catch (InterruptedException iexError) {
+                System.out.println("Hubo un error en el juego " + 
+                        iexError.toString());
+            }
+	}
+    }
+    
+    public void actualiza (){
 
+    }
+    
+    /**
+     * checaColision
+     * 
+     * Metodo usado para checar la colision entre objetos
+     * 
+     */
+    public void checaColision () {
+
+    }
+    
+        /**
+     * update
+     * 
+     * Metodo sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo lo que hace es actualizar el contenedor y 
+     * define cuando usar ahora el paint
+     * 
+     * @param graGrafico es el <code>objeto grafico</code> usado para dibujar.
+     * 
+     */
+    public void paint (Graphics graGrafico){
+        // Inicializan el DoubleBuffer
+        if (imaImagenApplet == null){
+                imaImagenApplet = createImage (this.getSize().width, 
+                        this.getSize().height);
+                graGraficaApplet = imaImagenApplet.getGraphics ();
+        }
+
+        // Actualiza la imagen de fondo.
+        URL urlImagenFondo = this.getClass().getResource("Ciudad.png");
+        Image imaImagenFondo = Toolkit.getDefaultToolkit().getImage(urlImagenFondo);
+         graGraficaApplet.drawImage(imaImagenFondo, 0, 0, iWidth, iHeight, this);
+
+        // Actualiza el Foreground.
+        graGraficaApplet.setColor (getForeground());
+        paint1(graGraficaApplet);
+
+        // Dibuja la imagen actualizada
+        graGrafico.drawImage (imaImagenApplet, 0, 0, this);
+    }
+
+    /**
+     * paint1
+     * 
+     * Metodo sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo se dibuja la imagen con la posicion actualizada,
+     * ademas que cuando la imagen es cargada te despliega una advertencia.
+     * 
+     * @param graDibujo es el objeto de <code>Graphics</code> usado para dibujar.
+     * 
+     */
+    public void paint1 (Graphics graDibujo) {
+        // si la imagen ya se cargo
+        if (llsJuanito != null && basMalo != null && llsFantasmas != null) {
+            if (bVivo){
+                //Dibuja la imagen de principal en el Applet
+                for(Base basJuanito : llsJuanito){
+                        basJuanito.paint(graDibujo, this);
+                }
+                //Dibuja la imagen de malo en el Applet
+                basMalo.paint(graDibujo, this);
+                //Dibujando los fantasmas
+                for(Base basFantasma : llsFantasmas){
+                        basFantasma.paint(graDibujo, this);
+                }
+                //Puntos y vidas desplegados en la esquina superior izquierda
+                graDibujo.setColor(Color.red);
+                graDibujo.drawString("Puntos: " + iPuntos, 15, 45);
+                graDibujo.drawString ("Vidas: " + iVidas, 15, 60);
+            }
+            else{
+                basGameOver.paint(graDibujo, this);
+            }
+        } // sino se ha cargado se dibuja un mensaje 
+        else {
+                //Da un mensaje mientras se carga el dibujo	
+                graDibujo.drawString("No se cargo la imagen..", 20, 20);
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
